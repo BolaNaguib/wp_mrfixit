29f35e6e7856b3a8f42afb74a7b2eec3929cfc70
Bola Naguib <bolanaguib@gmail.com>
2020-02-11 22:03:34 +0200

    [ Changed uikit icons to defer ]

--- src/template/footer.php
+++ src/template/footer.php
@@ -112,9 +112,9 @@ style="  background-image: url('<?php echo get_template_directory_uri() ;?>/imag
 <!-- END footer -->
 
 <!-- UIkit JS -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script async src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 <script async src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.3/js/uikit.min.js"></script>
-<script async src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.3/js/uikit-icons.min.js"></script>
+<script defer src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.3/js/uikit-icons.min.js"></script>
 
 
 <?php wp_footer() ?>
