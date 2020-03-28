1a12a8c9b7d32bb2e80a65c9a354b33147147d9f
Bola Naguib <bolanaguib@gmail.com>
2020-03-28 19:18:23 +0200

    fixed

--- header.php
+++ header.php
@@ -21,7 +21,7 @@
           <span class="" uk-navbar-toggle-icon>
 
           </span>
-        </a> --><li class="uk-hidden@m" style="list-style: none;"><a class="nav__link-quote" href="tel:<?php echo $phone; ?>"> <?php echo $phone_display; ?></a></li></div></div></div><!-- END uk-container --><!-- END nav_type-main --></nav><!-- Off Canvas --><!-- <div id="offcanvas-overlay" uk-offcanvas="overlay: true">
+        </a> --><li class="uk-hidden@l" style="list-style: none;"><a class="nav__link-quote" href="tel:<?php echo $phone; ?>"> <?php echo $phone_display; ?></a></li></div></div></div><!-- END uk-container --><!-- END nav_type-main --></nav><!-- Off Canvas --><!-- <div id="offcanvas-overlay" uk-offcanvas="overlay: true">
   <div class="uk-offcanvas-bar zxc section_theme_white">
 
     <button class="uk-offcanvas-close" type="button" uk-close></button>
