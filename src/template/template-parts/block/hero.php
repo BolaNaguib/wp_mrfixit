bf2e36587348ed2f67ece63d924407d3c135b374
Bola Naguib <bolanaguib@gmail.com>
2020-03-28 21:11:23 +0200

    [ fixed ] - hero

--- src/template/template-parts/block/hero.php
+++ src/template/template-parts/block/hero.php
@@ -28,7 +28,7 @@ $full_width = get_field('full_width');
       <h1> <?php echo $hero_title; ?> <?php echo $city; ?> </h1>
       <br>
       <div>
-        <a class="callback" href="<?php echo $phone; ?>"><?php echo $phone_display; ?></a>
+        <a class="callback" href="tel:<?php echo $phone; ?>"><?php echo $phone_display; ?></a>
       </div>
       <div>
         <br>
