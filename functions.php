63635afda2559558131cedecba63e41f1bdd2c39
Bola Naguib <bolanaguib@gmail.com>
2020-04-14 18:09:28 +0200

    [ removed ] google fonts

--- functions.php
+++ functions.php
@@ -10,7 +10,7 @@ function add_enqueue_styles(){
     // wp_enqueue_style( 'fontawesome',  'https://use.fontawesome.com/releases/v5.8.1/css/all.css');
     
     // wp_enqueue_style( 'uikit_css',  'https://cdn.jsdelivr.net/npm/uikit@3.3.7/dist/css/uikit.min.css');
-    wp_enqueue_style( 'google_font',  'https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap');
+    // wp_enqueue_style( 'google_font',  'https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap');
     wp_enqueue_script( 'uikit_js', 'https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.3/js/uikit.min.js', array(), false, true);
 }
 
