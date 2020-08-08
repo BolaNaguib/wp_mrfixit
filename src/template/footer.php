b6193ad2ff4dca1c82bf0cec0a8b7c5dd9c30df2
Bola <bolanaguib@gmail.com>
2020-08-08 22:06:17 +0200

    [ added ] mo opener to external links

--- src/template/footer.php
+++ src/template/footer.php
@@ -84,18 +84,18 @@ $footer = get_field('footer', $post_id);
                         <div class="uk-flex">
                             <ul class="uk-iconnav social">
 
-                                <li><a href="https://www.facebook.com/MrFixItGD/">
+                                <li><a rel="noopener" href="https://www.facebook.com/MrFixItGD/">
                                         <svg style="width: 20px;" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="facebook-square" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="svg-inline--fa fa-facebook-square fa-w-14">
                                             <path fill="currentColor" d="M400 32H48A48 48 0 0 0 0 80v352a48 48 0 0 0 48 48h137.25V327.69h-63V256h63v-54.64c0-62.15 37-96.48 93.67-96.48 27.14 0 55.52 4.84 55.52 4.84v61h-31.27c-30.81 0-40.42 19.12-40.42 38.73V256h68.78l-11 71.69h-57.78V480H400a48 48 0 0 0 48-48V80a48 48 0 0 0-48-48z" class=""></path>
                                         </svg>
                                     </a></li>
-                                <li><a href="https://www.google.com/search?q=mr+fix+it+garage+door&amp;rlz=1C1SQJL_enEG867EG867&amp;oq=mr&amp;aqs=chrome.1.69i57j69i59j69i60l3j69i61j69i60j69i65.9695j0j4&amp;sourceid=chrome&amp;ie=UTF-8"><svg style="width: 20px;" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="google" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 488 512" class="svg-inline--fa fa-google fa-w-16">
+                                <li><a rel="noopener" href="https://www.google.com/search?q=mr+fix+it+garage+door&amp;rlz=1C1SQJL_enEG867EG867&amp;oq=mr&amp;aqs=chrome.1.69i57j69i59j69i60l3j69i61j69i60j69i65.9695j0j4&amp;sourceid=chrome&amp;ie=UTF-8"><svg style="width: 20px;" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="google" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 488 512" class="svg-inline--fa fa-google fa-w-16">
                                             <path fill="currentColor" d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z" class=""></path>
                                         </svg></a></li>
-                                <li><a href="https://www.instagram.com/mrfixit_gd/"><svg style="width: 20px;" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="instagram" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="svg-inline--fa fa-instagram fa-w-14">
+                                <li><a rel="noopener" href="https://www.instagram.com/mrfixit_gd/"><svg style="width: 20px;" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="instagram" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="svg-inline--fa fa-instagram fa-w-14">
                                             <path fill="currentColor" d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z" class=""></path>
                                         </svg></a></li>
-                                <li><a href="yelp.com/biz/mr-fix-it-garage-door-boynton-beach-3"><svg style="width: 20px;" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="yelp" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512" class="svg-inline--fa fa-yelp fa-w-12">
+                                <li><a rel="noopener" href="yelp.com/biz/mr-fix-it-garage-door-boynton-beach-3"><svg style="width: 20px;" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="yelp" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512" class="svg-inline--fa fa-yelp fa-w-12">
                                             <path fill="currentColor" d="M42.9 240.32l99.62 48.61c19.2 9.4 16.2 37.51-4.5 42.71L30.5 358.45a22.79 22.79 0 0 1-28.21-19.6 197.16 197.16 0 0 1 9-85.32 22.8 22.8 0 0 1 31.61-13.21zm44 239.25a199.45 199.45 0 0 0 79.42 32.11A22.78 22.78 0 0 0 192.94 490l3.9-110.82c.7-21.3-25.5-31.91-39.81-16.1l-74.21 82.4a22.82 22.82 0 0 0 4.09 34.09zm145.34-109.92l58.81 94a22.93 22.93 0 0 0 34 5.5 198.36 198.36 0 0 0 52.71-67.61A23 23 0 0 0 364.17 370l-105.42-34.26c-20.31-6.5-37.81 15.8-26.51 33.91zm148.33-132.23a197.44 197.44 0 0 0-50.41-69.31 22.85 22.85 0 0 0-34 4.4l-62 91.92c-11.9 17.7 4.7 40.61 25.2 34.71L366 268.63a23 23 0 0 0 14.61-31.21zM62.11 30.18a22.86 22.86 0 0 0-9.9 32l104.12 180.44c11.7 20.2 42.61 11.9 42.61-11.4V22.88a22.67 22.67 0 0 0-24.5-22.8 320.37 320.37 0 0 0-112.33 30.1z" class=""></path>
                                         </svg></a></li>
                             </ul>
@@ -143,7 +143,7 @@ $footer = get_field('footer', $post_id);
                 <div class='card_type-footer'>
                     <h3>LOCATION</h3>
                     <?php if ($mapImage) : ?>
-                        <a href="<?php echo $mapImageURL ?>">
+                        <a rel="noopener" href="<?php echo $mapImageURL ?>">
                             <!-- START mapiframe -->
                             <div class='mapiframe' style="background-image: url('<?php echo $mapImage['url'] ?>');   min-height: 200px;
     background-position: center;
@@ -206,7 +206,7 @@ $footer = get_field('footer', $post_id);
 <div class='uk-text-center'>
 
     <img src="<?php echo $paymentmethods['url']; ?>" alt="<?php echo $paymentmethods['alt']; ?>" title="<?php echo $paymentmethods['title']; ?>"><br>
-    <small> <?php echo $copyrights; ?> | Developed by <a style="color:#fff;">xvxlabs.com</a></small>
+    <small> <?php echo $copyrights; ?> | Developed by <a rel="noopener" style="color:#fff;">xvxlabs.com</a></small>
 </div>
 <!-- END uk-text-center -->
             </div>
